55d99d2473e3a523874ccb140b22bb6089d3cbb4
pawieee <p.agujitas.534827@umindanao.edu.ph>
2025-04-15 17:18:38 +0800

    i think working na AHAHAH

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -13,6 +13,7 @@ export default function LoginPage() {
       const response = await fetch("http://localhost:5000/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
+        credentials: "include",
         body: JSON.stringify({ username, password }),
       });
 
