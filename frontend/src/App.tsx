9016019bf3233f0f648b124de1484428c10d6203
Cristian Jay <kencarlneyra@gmail.com>
2025-04-20 04:08:32 +0800

    added: loan application table to the view

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,7 +10,6 @@ import Login from "./pages/Login";
 import Page from "./pages/Page";
 import { useView, ViewProvider } from "./context/ViewContext";
 import { AlertProvider } from "./context/AlertContext";
-import TestPage from "./pages/TestPage"
 
 function App() {
   const location = useLocation();
@@ -30,7 +29,6 @@ function App() {
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/page" element={<Page />} />
-      <Route path="/test" element={<TestPage />} />
     </Routes>
   );
 }
